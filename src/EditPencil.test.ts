effac643616d8b9095369537ed7cb007267016e0
someone <someone>
2020-06-07 11:09:38 -0500

    Create edit feature tests: red

---
+++ src/EditPencil.test.ts
@@ -0,0 +1,48 @@
+// As a writer
+// I want to be able to erase previously written text
+// so that I can remove my mistakes
+
+import Paper from './Paper';
+import Pencil from './Pencil';
+
+// overflowing edit turns into append
+describe('The ability for a pencil to edit', () => {
+    it('replaces space or newline characters with given text ', () => {
+        const page = new Paper();
+        const pencil = new Pencil(200, 10);
+
+        pencil.writeOnPaper('    \n     ', page);
+        pencil.edit(3, 'apple', page);
+
+        expect(page.getPageContents()).toBe('   apple  ');
+    });
+
+    it('replaces overlapping characters with an @ symbol', () => {
+        const page = new Paper();
+        const pencil = new Pencil(200, 10);
+
+        pencil.writeOnPaper('codekata', page);
+        pencil.edit(0, 'conflict', page);
+
+        expect(page.getPageContents()).toBe('@@@@@@@@');
+    });
+    it("if an edit overflows the page's initial content, the content is extended", () => {
+        const page = new Paper();
+        const pencil = new Pencil(200, 10);
+
+        pencil.writeOnPaper('code', page);
+        pencil.edit(0, 'overflow', page);
+
+        expect(page.getPageContents()).toBe('@@@@flow');
+    });
+    it("throws an error if an index is given which doesn't exist", () => {
+        const page = new Paper();
+        const pencil = new Pencil(200, 10);
+
+        const badIndex = () => {
+            pencil.edit(5, 'dog', page);
+        };
+
+        expect(badIndex).toThrowError();
+    });
+});
